e267d574b2e45f3c54aa9fd5994fdf22a73c7897
Bob Niu <spartucus@users.noreply.github.com>
2023-05-16 16:36:45 +0800

    finish

--- src/blake2b.rs
+++ src/blake2b.rs
@@ -1,16 +1,16 @@
 use crate::traits::Hasher;
-use blake2::{digest::FixedOutput, Blake2bMac};
-use digest::{consts::U32, Mac};
+// use blake2::{digest::FixedOutput, Blake2b};
+// use digest::{consts::U32, Update};
+use blake2_rfc::blake2b::Blake2b;
+// pub type Blake2b256 = Blake2b<U32>;
 
-pub type Blake2bMac256 = Blake2bMac<U32>;
+pub struct Blake2bHasher(Blake2b);
 
-pub struct Blake2bHasher(Blake2bMac256);
-
-const PERSONALIZATION: &[u8] = b"sparsemerkletree";
+// const PERSONALIZATION: &[u8] = b"sparsemerkletree";
 
 impl Default for Blake2bHasher {
     fn default() -> Self {
-        let blake2b = Blake2bMac256::new_with_salt_and_personal(&[], &[], PERSONALIZATION).unwrap();
+        let blake2b = Blake2b::new(32);
         Self(blake2b)
     }
 }
@@ -25,7 +25,7 @@ impl Hasher for Blake2bHasher {
     }
 
     fn finish(self) -> crate::h256::H256 {
-        let out: [u8; 32] = self.0.finalize_fixed()[..].try_into().expect("slice with incorrect length");
+        let out: [u8; 32] = self.0.finalize().as_bytes().try_into().expect("finish failed");
         out.into()
     }
 }
--- tests/blake.rs
+++ tests/blake.rs
@@ -1,10 +1,6 @@
-use std::hash;
-
-use blake2::{digest::FixedOutput, Blake2bMac};
 use hex_literal::hex;
 use blake2b_rs::Blake2bBuilder;
-use digest::consts::U32;
-use verify_smt::{blake2b::{Blake2bMac256, Blake2bHasher as SMTBlake2b}, traits::Hasher, h256::H256};
+use blake2_rfc::blake2b::Blake2b;
 
 #[test]
 #[rustfmt::skip]
@@ -13,44 +9,32 @@ fn blake2b_with_key() {
         000102030405060708090a0b0c0d0e0f
         101112131415161718191a1b1c1d1e1f
     ");
-    
-    let persona = b"personal";
-    let ctx = Blake2bMac256::new_with_salt_and_personal(&key, &[], persona).unwrap();
-    let out: [u8; 32] = ctx.finalize_fixed()[..].try_into().expect("slice with incorrect length");
-    println!("blake2: {:?}", out);
-    // [216, 141, 253, 59, 178, 225, 251, 57, 240, 43, 71, 16, 202, 233, 139, 24, 125, 156, 220, 66, 65, 239, 91, 142, 82, 244, 220, 155, 221, 254, 57, 126]
 
-    let mut out = [0u8; 32];
-    let mut hasher = Blake2bBuilder::new(32).salt(&[]).personal(persona).key(&key).build();
-    hasher.finalize(&mut out);
-    println!("blake2b-rs: {:?}", out);
-    // [216, 141, 253, 59, 178, 225, 251, 57, 240, 43, 71, 16, 202, 233, 139, 24, 125, 156, 220, 66, 65, 239, 91, 142, 82, 244, 220, 155, 221, 254, 57, 126]
+    let ctx = Blake2b::with_key(32, &key);
+    let out0: [u8; 32] = ctx.finalize().as_bytes().try_into().expect("slice with incorrect length");
+    println!("blake2-rfc: {:?}", out0);
+    // [78, 81, 231, 169, 19, 252, 128, 19, 125, 165, 40, 128, 254, 204, 161, 117, 191, 129, 225, 23, 213, 198, 129, 38, 220, 39, 116, 3, 53, 23, 234, 13]
+
+    let mut out1 = [0u8; 32];
+    let hasher = Blake2bBuilder::new(32).key(&key).build();
+    hasher.finalize(&mut out1);
+    println!("blake2b-rs: {:?}", out1);
+    // [78, 81, 231, 169, 19, 252, 128, 19, 125, 165, 40, 128, 254, 204, 161, 117, 191, 129, 225, 23, 213, 198, 129, 38, 220, 39, 116, 3, 53, 23, 234, 13]
 }
 
 #[test]
 fn blake2b_without_key() {
     const PERSONALIZATION: &[u8] = b"sparsemerkletree";
-    let salt: &[u8] = &[];
-    let key: &[u8] = &[];
-    let ctx = Blake2bMac256::new_with_salt_and_personal(key, salt, PERSONALIZATION).unwrap();
-    let out: [u8; 32] = ctx.finalize_fixed()[..].try_into().expect("slice with incorrect length");
-    println!("blake2: {:?}", out);
-    // [146, 200, 238, 190, 99, 195, 209, 158, 61, 186, 253, 78, 231, 206, 122, 240, 212, 148, 183, 192, 123, 169, 250, 191, 184, 108, 209, 54, 138, 246, 132, 144]
+    let mut ctx = Blake2b::new(32);
+    ctx.update(PERSONALIZATION);
+    let out: [u8; 32] = ctx.finalize().as_bytes().try_into().expect("slice with incorrect length");
+    println!("blake2-rfc: {:?}", out);
+    // [20, 40, 150, 3, 106, 99, 194, 240, 215, 8, 239, 35, 138, 184, 222, 228, 253, 91, 178, 51, 19, 207, 89, 164, 252, 54, 49, 3, 232, 160, 39, 158]
 
     let mut out = [0u8; 32];
-    let mut hasher = Blake2bBuilder::new(32).key(key).salt(salt).personal(PERSONALIZATION).build();
+    let mut hasher = Blake2bBuilder::new(32).build();
+    hasher.update(PERSONALIZATION);
     hasher.finalize(&mut out);
     println!("blake2b-rs: {:?}", out);
-    // [86, 76, 113, 0, 182, 253, 107, 161, 250, 165, 230, 88, 221, 238, 116, 125, 65, 247, 12, 41, 121, 183, 107, 240, 56, 1, 197, 104, 91, 49, 237, 252]
-}
-
-#[test]
-fn blake2b_hasher() {
-    let b = 1_u8;
-    let h = H256::from([0; 32]);
-    let mut hasher = SMTBlake2b::default();
-    // hasher.write_byte(b);
-    // hasher.write_h256(&h);
-    let r = hasher.finish();
-    println!("r: {:?}", r);
+    // [20, 40, 150, 3, 106, 99, 194, 240, 215, 8, 239, 35, 138, 184, 222, 228, 253, 91, 178, 51, 19, 207, 89, 164, 252, 54, 49, 3, 232, 160, 39, 158]
 }
