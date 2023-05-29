398a898698ebac8aadd20594ecdf53c18f0a306f
penghuaping <lansheng228@163.com>
2023-05-29 17:51:41 +0800

    format code

--- src/merkle_verify.rs
+++ src/merkle_verify.rs
@@ -8,7 +8,7 @@ pub struct MerkleVerify;
 
 impl MerkleVerify {
     pub fn verify_proof(proof: &str, root: &str, leaves: &Vec<(&str, &str)>) -> bool {
-        let compiled_proof = hex::decode(proof).unwrap();
+        let proof = hex::decode(proof).unwrap();
         let root: [u8; 32] = hex::decode(root).unwrap().try_into().expect("failed");
         let leaves = leaves.into_iter().map(|l| {
             let left: [u8; 32] = hex::decode(l.0).unwrap().try_into().expect("failed");
@@ -16,7 +16,7 @@ impl MerkleVerify {
             (H256::from(left), H256::from(right))
         }).collect::<Vec<_>>();
 
-        let proof = CompiledMerkleProof(compiled_proof);
+        let proof = CompiledMerkleProof(proof);
         let root = H256::from(root);
         let result = proof.verify::<Blake2bHasher>(&root, leaves).unwrap();
 
