4fb453cecb80b37c8b10fdab452c5e5ececcd797
simbahebinbo <lansheng228@163.com>
2023-07-12 16:00:08 +0800

    update merkle verify

--- src/merkle_verify.rs
+++ src/merkle_verify.rs
@@ -1,3 +1,4 @@
+use blake2_rfc::blake2b::Blake2b;
 use hex;
 
 use crate::blake2b::Blake2bHasher;
@@ -9,10 +10,12 @@ pub struct MerkleVerify;
 impl MerkleVerify {
     pub fn verify_proof(proof: &str, root: &str, leaves: &Vec<(&str, &str)>) -> bool {
         let proof = hex::decode(proof).unwrap();
-        let root: [u8; 32] = hex::decode(root).unwrap().try_into().expect("failed");
+        let root: [u8; 32] = hex::decode(root).unwrap().try_into().unwrap();
         let leaves = leaves.into_iter().map(|l| {
-            let left: [u8; 32] = hex::decode(l.0).unwrap().try_into().expect("failed");
-            let right: [u8; 32] = hex::decode(l.1).unwrap().try_into().expect("failed");
+            let mut blake2b = Blake2b::new(32);
+            blake2b.update(hex::decode(l.0).unwrap().as_slice().clone());
+            let left: [u8; 32] = blake2b.finalize().as_bytes().try_into().unwrap();
+            let right: [u8; 32] = hex::decode(l.1).unwrap().try_into().unwrap();
             (H256::from(left), H256::from(right))
         }).collect::<Vec<_>>();
 
@@ -25,10 +28,12 @@ impl MerkleVerify {
 
     pub fn merkle_verify(proof: String, root: String, leaves: Vec<(String, String)>) -> bool {
         let proof = hex::decode(proof).unwrap();
-        let root: [u8; 32] = hex::decode(root).unwrap().try_into().expect("failed");
+        let root: [u8; 32] = hex::decode(root).unwrap().try_into().unwrap();
         let leaves = leaves.into_iter().map(|l| {
-            let left: [u8; 32] = hex::decode(l.0).unwrap().try_into().expect("failed");
-            let right: [u8; 32] = hex::decode(l.1).unwrap().try_into().expect("failed");
+            let mut blake2b = Blake2b::new(32);
+            blake2b.update(hex::decode(l.0).unwrap().as_slice().clone());
+            let left: [u8; 32] = blake2b.finalize().as_bytes().try_into().unwrap();
+            let right: [u8; 32] = hex::decode(l.1).unwrap().try_into().unwrap();
             (H256::from(left), H256::from(right))
         }).collect::<Vec<_>>();
 
